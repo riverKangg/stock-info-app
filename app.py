88f6760c858b92ffc700daf11771254edcd57e30
riverkangg <riverkangg@gmail.com>
2023-10-25 02:51:07 +0900

    Added files.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# 루트 URL에 대한 뷰 함수
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', message="Welcome to the Stock Info App")
+
+# "about" URL에 대한 뷰 함수
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- stock_info/__init__.py
+++ stock_info/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from stock_info import routes #, models
 
 # Create the Flask application instance
 app = Flask(__name__)
